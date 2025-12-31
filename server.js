11bb178f214d26a0b80e3cb8b24fb41050a6c1e1
rpannieong <annie_ong@myrp.edu.sg>
2025-12-31 11:41:31 +0800

    Added updatecard service

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.listen(port, () => {
 app.get('/allcards', async (req, res) => {
    try{
        let connection = await mysql.createConnection(dbConfig);
-       const[rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+       const[rows] = await connection.execute('SELECT * FROM cards');
        res.json(rows);
    } catch (err) {
         console.error(err);
@@ -45,6 +45,19 @@ app.post('/addcard', async (req, res) => {
         let connection = await mysql.createConnection(dbConfig);
         await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?, ?)', [card_name, card_pic]);
         res.status(201).json({ message: 'Card ' + card_name + ' added successfully!' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error - could not add card ' + card_name });
+    }
+});
+
+// Update card
+app.post('/updatecard', async (req, res) => {
+    const { id, card_name, card_pic } = req.body;
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('UPDATE cards SET card_name=?, card_pic=? WHERE id=?', [card_name, card_pic, id]);
+        res.status(201).json({ message: 'Card ' + card_name + ' updated successfully!' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error - could not add card ' + card_name });
